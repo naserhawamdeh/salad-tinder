521b0e5250ab8c51b597c2bb98cb85e85d28b4c9
gsomer <gasomer75@hotmail.com>
2022-04-04 17:22:05 -0700

    add User model and user-routes

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { Model, DataTypes } = require('sequelize');
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 const sequelize = require('../config/connection');
 
 class User extends Model {
@@ -32,7 +32,7 @@ User.init(
             type: DataTypes.STRING,
             allowNull: false,
             validate: {
-                len: [4]
+                len: [8, 24]
             }
         }
     },
--- server.js
+++ server.js
@@ -4,3 +4,19 @@ const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
+const { urlencoded } = require('express');
+
+// assign express function call to 'app' variable
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// establish routes
+app.use(routes);
+
+// connection to db and start server
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
