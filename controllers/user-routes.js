4fc49018aac2da4b5657e6cd0664a587a373e8c0
dimas <dprcontractor@gmail.com>
2022-04-05 22:31:10 -0400

    initial commit

--- controllers/user-routes.js
+++ controllers/user-routes.js
@@ -1,2 +1,123 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User } = require('../models');
+
+// GET all users
+// http://localhost:3001/users
+router.get('/', (req, res) => {
+  User.findAll({
+    // removes password from results
+    attributes: { exclude: ['password'] }
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// GET a single user
+// http://localhost:3001/users/<id>
+router.get('/:id', (req, res) => {
+  User.findOne({
+    // removes password from results
+    attributes: { exclude: ['password'] },
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: "User not found."});
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST create a new user
+// http://localhost:3001/users
+router.post('/', (req, res) => {
+  User.create({
+    username: req.body.username,
+    email: req.body.email,
+    password: req.body.password
+  })
+    .then(dbUserData => res.json(dbUserData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// POST (authenticate user by email/password)
+// **MUST HAVE 'EMAIL' AND 'PASSWORD' FIELDS TO OPERATE CORRECTLY
+// http://localhost:3001/users/login
+router.post('/login', (req, res) => {
+    // check for email address associated with a user
+  User.findOne({
+      where: {
+        email: req.body.email
+      }
+    }).then(dbUserData => {
+      if (!dbUserData) {
+        res.status(400).json({ message: 'Email address not found.' });
+        return;
+      }
+      // Verify user password
+      const validPassword = dbUserData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password.' });
+        return;
+      }
+      res.json({ user: dbUserData, message: 'Login successful.' });
+    });  
+  });
+
+// PUT update a user
+// http://localhost:3001/users/<id>
+router.put('/:id', (req, res) => {
+  User.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData[0]) {
+        res.status(404).json({ message: 'User not found.' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// DELETE a user
+// http://localhost:3001/users/<id>
+router.delete('/:id', (req, res) => {
+  User.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData) {
+        res.status(404).json({ message: 'User not found.' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+
+module.exports = router;
